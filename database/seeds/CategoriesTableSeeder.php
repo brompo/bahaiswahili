0744eb7d6e7768a8f69614cbc50ffc6d5292c691
brompo <brompo@gmail.com>
2018-01-21 15:24:08 +0300

    added a todo file, tracking tasks, and added it to git ignore file

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,6 +12,6 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $categories = factory(Category::class, 10)->create();
+        $categories = factory(Category::class, 5)->create();
     }
 }
